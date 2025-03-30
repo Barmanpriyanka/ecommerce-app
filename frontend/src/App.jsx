0838002730d0cf71b2db7511978f30cdaa51cdfa
Barmanpriyanka <priyankabarman1604@gmail.com>
2025-03-30 22:20:01 +0530

    Included tailwind and vite in the peoject.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
 const App = () => {
   return (
-    <div>App</div>
+    <div className='px-4sm:px-[5vw] md:px-[7vw] 1g:px-[9vw]'>
+      <Routes>
+        <Route path='/' element={Home}/>
+        <Route path='/collection' element={<Collection/>}/>
+        <Route  path='/about' element={<About/>}/>
+        <Route path='/contact' elment={<Contact/>}/>
+        <Route path='/product/:productId' element={<Product/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/place-order' element={<PlaceOrder/>} />
+      </Routes>
+    </div>
+    
   )
 }
 
